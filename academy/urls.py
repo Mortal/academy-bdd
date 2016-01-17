d3d04d09da44c8ea9e10649fd320365e23673197
Mathias Rav <FORM13@TAAGEKAMMERET.dk>
2016-01-17 13:15:51 +0100

    Update project skeleton according to Django 1.9.1

--- academy/urls.py
+++ academy/urls.py
@@ -1,14 +1,25 @@
-from django.conf.urls import patterns, include, url
+"""academy URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
 from django.contrib import admin
 
-from . import views
+from academy import views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'academy.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
 
     url(r'^$', views.FrontView.as_view(), name='front'),
     url(r'^game/$', views.GameListView.as_view(), name='game_list'),
@@ -20,4 +31,4 @@ urlpatterns = patterns('',
     url(r'^player/$', views.PlayerListView.as_view(), name='player_list'),
     url(r'^player/new/$', views.PlayerCreateView.as_view(), name='player_create'),
     url(r'^player/(?P<pk>\d+)/$', views.PlayerView.as_view(), name='player'),
-)
+]
